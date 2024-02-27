f2fd5fdd61ba754cddc16b65c3bbb04a2efc174b
lakepier <lakeehan@proton.me>
2024-02-27 23:56:24 +0300

    Initial commit

---
+++ build_kernel.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+export CROSS_COMPILE=/home/lakepier/Templates/toolchains/toolchain_cross-compile/bin/
+
+export ARCH=arm64
+export PLATFORM_VERSION=12
+export ANDROID_MAJOR_VERSION=s
+export CONFIG_SECTION_MISMATCH_WARN_ONLY=y
+
+make ARCH=arm64 exynos9810-r7_defconfig
+make ARCH=arm64
